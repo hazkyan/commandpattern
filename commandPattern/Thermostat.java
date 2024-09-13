d81d764cd21f2311ad9e93fef1da9222ae8071cd
hazkyan <hazpoldev@gmail.com>
2024-09-13 21:13:35 +0800

    Update Thermostat.java

--- commandPattern/Thermostat.java
+++ commandPattern/Thermostat.java
@@ -9,12 +9,4 @@ public class Thermostat {
     public void turnOff(){
         System.out.println("Thermostat is turned off!");
     }
-
-    public void increaseTemp(){
-        System.out.println("Increased Temperature!");
-    }
-
-    public void decreaseTemp(){
-        System.out.println("Decreased Temperature!");
-    }
 }
